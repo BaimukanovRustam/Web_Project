517a76687e02cbd69be391cbc5035a1315754c0e
BaimukanovRustam <43208351+BaimukanovRustam@users.noreply.github.com>
2018-12-17 15:01:46 +0600

    Delete Registration_Form ver 1.php
    Remove file

--- Registration_Form ver 1.php
+++
@@ -1,6 +0,0 @@
-<form action="Data_Form ver 1.php" method="post">
- <p>Name: <input type="text" name="name" /></p>
- <p>Surname: <input type="text" name="surname" /></p>
- <p>Age: <input type="text" name="age" /></p>
- <p><input type="submit" /></p>
-</form>
